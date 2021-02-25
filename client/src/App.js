ee2879f7517a10ddbb6f5b17012fa607d461f7bb
Sungpil An <ansungpil1@gmail.com>
2021-02-25 14:31:06 -0500

    added app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import { useEffect } from "react";
-import axios from "axios";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./containers/Home/Home";
+import Main from "./containers/Main/Main";
+import Add from "./containers/Add/Add";
+import Edit from "./containers/Edit/Edit";
+import NavBar from "./components/NavBar/NavBar";
+
 
 function App() {
-
-  useEffect(() => {
-    axios
-    .get("/api/config")
-    .then(response => {
-      console.log(response.data);
-    }).catch((err) => {
-      console.log(err);
-    });
-  }, []);
   return (
     <div className="App">
-      Sup Dude
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/main" component={Main} />
+          <Route exact path="/main/new" component={Add} />
+          <Route exact path="/main/:id/edit" component={Edit} />
+        </Switch>
+      </Router>
     </div>
   );
 }
