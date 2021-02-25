531a666226af74998a8b892668504a2c4d1e7069
Sungpil An <ansungpil1@gmail.com>
2021-02-25 16:17:26 -0500

    added user authentication method

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+
+import React from 'react';
+import {Link} from "react-router-dom";
 
 const NavBar = () => {
     return (
         <div>
-            Nav
+            <Link to="/">Home</Link>
+            <Link to="/login">Log-In</Link>
+            <Link to="/main">My Inventory</Link>
+            <Link to="/main/new">Add New Item</Link>
         </div>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
