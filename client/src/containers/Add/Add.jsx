cb20b48c853765ff766ea595f552460b0e23fd6e
Sungpil An <ansungpil1@gmail.com>
2021-02-25 15:46:13 -0500

    added Add page

--- client/src/containers/Add/Add.jsx
+++ client/src/containers/Add/Add.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import ItemForm from "../../components/FoodForm/FoodForm";
 
 const Add = () => {
+    const history = useHistory();
+
+    const handleFormSubmit = (e, formObject) => {
+        e.preventDefault();
+        axios
+            .post("/api/main", formObject)
+            .then((response) => {
+                console.log(response.data);
+                history.push(`/main/${response.data_id}`);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
     return (
-        <div>
-            Add Page
+        <div className="container">
+            <div className="row">
+                <ItemForm 
+                    handleFormSubmit={handleFormSubmit}
+                    buttonText="Add New Item"
+                />
+            </div>
         </div>
     )
 }
 
-export default Add
+export default Add;
